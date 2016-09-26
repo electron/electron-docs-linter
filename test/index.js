8c29672d1db9574c790f9616d35d8b1a1e015776
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-26 15:15:12 -0700

    standardize

--- test/index.js
+++ test/index.js
@@ -128,7 +128,7 @@ describe('APIs', function () {
       // expect(apis.BrowserWindow.instanceMethods.setSize.parameters.animate.required).to.eq(false)
     // })
 
-    they('can have descriptions that span multiple lines', function() {
+    they('can have descriptions that span multiple lines', function () {
       var method = apis.BrowserWindow.instanceMethods.setAspectRatio.parameters.extraSize
       expect(method.description).to.equal('(optional) - The extra size not to be included while maintaining the aspect ratio.')
     })
